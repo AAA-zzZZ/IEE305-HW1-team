88c15b821e628f4b6746da1dc7693a215f55bc95
AAA-zzZZ <ezheng3@asu.edu>
2025-02-02 05:33:37 -0700

    changes in index.html and task1_2

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
-from fastapi import FastAPI
-from datetime import datetime
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from datetime import date, datetime
+from pydantic import BaseModel, Field
 from fastapi.staticfiles import StaticFiles
 
 app=FastAPI()
 
 class HeatPump(BaseModel):
     name: str
-    serial_num: int
-    install_date: datetime
+    serial_num: float = Field(gt=0)
+    install_date: date
 
 
 app.heat_pump = HeatPump(
     name = "default",
     serial_num=1,
-    install_date=datetime(2000, 1, 1)
+    install_date=date(2000, 1, 1)
 )
 
 @app.get("/heat_pump")
 def get_heat_pump() ->HeatPump:
+    if app.heat_pump is None:
+        raise HTTPException(404, "Not found pump")
     return app.heat_pump
+
 
 @app.put("/heat_pump")
 def put_heat_pump(pump: HeatPump):
     app.heat_pump = pump
+
+@app.delete("/heat_pump")
+def delete_heat_pump():
+    app.heat_pump = None
 
 app.mount(
     "/", StaticFiles(directory=".", html=True)
