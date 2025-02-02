24c82a06ec7153f106e8a0ae646b8855565c811a
AAA-zzZZ <ezheng3@asu.edu>
2025-02-01 20:56:50 -0700

    updated main.py

--- main.py
+++ main.py
@@ -5,22 +5,25 @@ from fastapi.staticfiles import StaticFiles
 
 app=FastAPI()
 
-heat_pump=None
-
 class HeatPump(BaseModel):
     name: str
     serial_num: int
     install_date: datetime
 
 
+app.heat_pump = HeatPump(
+    name = "default",
+    serial_num=1,
+    install_date=datetime(2000, 1, 1)
+)
+
 @app.get("/heat_pump")
-def get_heat_pump():
-    return heat_pump
+def get_heat_pump() ->HeatPump:
+    return app.heat_pump
 
 @app.put("/heat_pump")
 def put_heat_pump(pump: HeatPump):
-    global heat_pump
-    heat_pump = pump
+    app.heat_pump = pump
 
 app.mount(
     "/", StaticFiles(directory=".", html=True)
